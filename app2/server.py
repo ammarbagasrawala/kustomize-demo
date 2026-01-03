3861d8d21976bd51b03562379456a8373549421e
ammarbagasrawala <ammariddris@gmail.com>
2026-01-04 00:47:28 +0530

    kustomize kube demo

---
+++ app2/server.py
@@ -0,0 +1,9 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello from App 2")
+
+HTTPServer(("", 8080), Handler).serve_forever()
